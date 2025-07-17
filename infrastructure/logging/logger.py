56608726ceca11ccd5a590c676b42ba7e0269b55
Michel Silva <michel.gomes.silva@gmail.com>
2025-07-17 22:26:46 +0100

    Fixed logger

--- infrastructure/logging/logger.py
+++ infrastructure/logging/logger.py
@@ -27,14 +27,6 @@ from loguru import logger
 from configs.settings import Settings
 
 # ----------------------------------------
-# Diretório base para armazenar arquivos de log
-LOG_DIR = Path(Settings.get_local_log_path())
-LOG_DIR.mkdir(exist_ok=True)
-
-# Caminho completo do arquivo de log baseado na data atual
-log_file = LOG_DIR / f"{datetime.now().strftime('%Y-%m-%d')}.log"
-
-# ----------------------------------------
 # Configuração do logger
 # Remove qualquer handler padrão do loguru para evitar logs duplicados
 logger.remove()
@@ -46,17 +38,25 @@ logger.add(
     colorize=True,  # terminal com cor
 )
 
-# Adiciona saída para arquivo com configurações de produção
-logger.add(
-    str(log_file),
-    level="DEBUG",  # nível mínimo DEBUG (mais detalhado)
-    rotation="10 MB",  # rotaciona após 10 MB
-    retention="10 days",  # mantém arquivos por até 10 dias
-    compression="zip",  # compacta arquivos antigos
-    backtrace=True,  # imprime stacktrace completo para exceções
-    diagnose=True,  # inclui informações extras sobre variáveis em erro
-    encoding="utf-8",  # evita problemas com acentuação
-)
+if Settings.APP_ENV == "dev":
+    # ----------------------------------------
+    # Diretório base para armazenar arquivos de log
+    LOG_DIR = Path(Settings.get_local_log_path())
+    LOG_DIR.mkdir(exist_ok=True)
+
+    # Caminho completo do arquivo de log baseado na data atual
+    log_file = LOG_DIR / f"{datetime.now().strftime('%Y-%m-%d')}.log"
+    # Adiciona saída para arquivo com configurações de produção
+    logger.add(
+        str(log_file),
+        level="DEBUG",  # nível mínimo DEBUG (mais detalhado)
+        rotation="10 MB",  # rotaciona após 10 MB
+        retention="10 days",  # mantém arquivos por até 10 dias
+        compression="zip",  # compacta arquivos antigos
+        backtrace=True,  # imprime stacktrace completo para exceções
+        diagnose=True,  # inclui informações extras sobre variáveis em erro
+        encoding="utf-8",  # evita problemas com acentuação
+    )
 
 # ----------------------------------------
 logger.info("Logger inicializado com sucesso.")
