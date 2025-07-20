74111d32039ef69ba665871c9a7c9c302e20dfd3
Michel Silva <michel.gomes.silva@gmail.com>
2025-07-20 14:48:53 +0100

    Ajusted vehicles with all fields

--- domain/schemas/vehicle_schema.py
+++ domain/schemas/vehicle_schema.py
@@ -10,13 +10,18 @@ from pyspark.sql.types import (
 vehicle_schema = StructType(
     [
         StructField("vehicle_id", StringType(), True),
-        StructField("line_id", StringType(), True),
+        StructField("line_id", IntegerType(), True),
+        StructField("trip_id", StringType(), True),
+        StructField("pattern_id", StringType(), True),
+        StructField("route_id", StringType(), True),
+        StructField("shift_id", StringType(), True),
+        StructField("stop_id", IntegerType(), True),
         StructField("latitude", DoubleType(), True),
         StructField("longitude", DoubleType(), True),
-        StructField("timestamp", LongType(), True),
-        StructField("CURRENT_STATUS", StringType(), True),
+        StructField("schedule_relationship", StringType(), True),
+        StructField("current_status", StringType(), True),
         StructField("speed", DoubleType(), True),
         StructField("direction", IntegerType(), True),
-        StructField("trip_id", StringType(), True),
+        StructField("timestamp", LongType(), True),
     ]
 )
