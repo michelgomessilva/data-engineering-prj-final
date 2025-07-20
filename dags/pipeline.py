47ca76e5a8e3b17bd04c1abf119978975ea92fde
Michel Silva <michel.gomes.silva@gmail.com>
2025-07-20 11:38:57 +0100

    Added config to auth in GC

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -2,8 +2,6 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.kubernetes.volume import Volume
-from airflow.kubernetes.volume_mount import VolumeMount
 from airflow.utils.task_group import TaskGroup
 
 # Configurações globais
@@ -12,28 +10,8 @@ REGION = "europe-west1"
 IMAGE_NAME = "grupo-2-pipeline-app"
 DOCKER_TAG = "latest"
 ARTIFACT_IMAGE = f"ghcr.io/michelsilva/{IMAGE_NAME}:{DOCKER_TAG}"
-DOCKER_CONFIG_PATH = "/root/.docker"
 
-# Volume para montar config.json
-volume_mount = VolumeMount(
-    name="docker-config-volume",
-    mount_path=DOCKER_CONFIG_PATH,
-    sub_path=None,
-    read_only=True,
-)
-
-volume = Volume(
-    name="docker-config-volume",
-    configs={
-        "gcs": {
-            "bucket": "europe-west1-airflow-196fdba9-bucket",
-            "object": "data/grupo-2/docker_auth_config.json",
-            "path": f"{DOCKER_CONFIG_PATH}/config.json",  # onde o arquivo será montado
-        }
-    },
-)
-
-# Argumentos padrão para todas as tasks
+# Argumentos padrão
 default_args = {
     "owner": "michelsilva",
     "depends_on_past": False,
@@ -45,11 +23,10 @@ default_args = {
     "start_date": datetime(2025, 7, 15),
 }
 
-# Criação da DAG principal
 with DAG(
     dag_id="pipeline_dag",
     default_args=default_args,
-    schedule_interval="12 */4 * * *",  # Executa a cada 4 horas
+    schedule_interval="12 */4 * * *",
     catchup=False,
     max_active_runs=1,
     concurrency=5,
@@ -57,7 +34,6 @@ with DAG(
     tags=["pipeline", "grupo-2"],
 ) as dag:
 
-    # TaskGroup: Use Cases Individuais
     with TaskGroup(
         "ingestion_tasks", tooltip="Ingestões individuais por dataset"
     ) as ingestion_group:
@@ -72,8 +48,7 @@ with DAG(
             get_logs=True,
             is_delete_operator_pod=True,
             env_vars={"APP_ENV": "production"},
-            volume_mounts=[volume_mount],
-            volumes=[volume],
+            image_pull_secrets=[{"name": "ghcr-docker-config"}],
         )
 
         ingest_municipalities = KubernetesPodOperator(
@@ -86,11 +61,9 @@ with DAG(
             get_logs=True,
             is_delete_operator_pod=True,
             env_vars={"APP_ENV": "production"},
-            volume_mounts=[volume_mount],
-            volumes=[volume],
+            image_pull_secrets=[{"name": "ghcr-docker-config"}],
         )
 
-    # Task: Executa todos os use-cases de uma só vez
     ingest_all = KubernetesPodOperator(
         task_id="ingest_all",
         name="ingest-all",
@@ -101,9 +74,7 @@ with DAG(
         get_logs=True,
         is_delete_operator_pod=True,
         env_vars={"APP_ENV": "production"},
-        volume_mounts=[volume_mount],
-        volumes=[volume],
+        image_pull_secrets=[{"name": "ghcr-docker-config"}],
     )
 
-    # Orquestração
     ingest_all >> ingestion_group
