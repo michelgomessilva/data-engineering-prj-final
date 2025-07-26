bf38b0fecc882329ca33d0b4b516ad6751bf70e9
Pedro Rocha <pedrohrp.rocha@gmail.com>
2025-07-26 12:47:16 +0100

    fix localities in routes

--- domain/cleaners/routes_cleanser.py
+++ domain/cleaners/routes_cleanser.py
@@ -29,7 +29,7 @@ def cleanse_routes_df(spark: SparkSession, input_path: str) -> DataFrame:
         upper(trim(col("long_name"))).alias("route_name"),
         upper(trim(col("color"))).alias("color"),
         upper(trim(col("text_color"))).alias("text_color"),
-        upper(trim(col("localities"))).alias("localities"),
+        col("localities"),
         col("municipalities"),
         col("patterns"),
         col("facilities"),
