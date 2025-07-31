bc261b7c0d37358f0d237fc16fca32c459a17e7c
telma_costa <telma.costa-ext@compass-group.fr>
2025-07-31 17:26:08 +0100

    fact_stops

--- dbt/models/grupo2_dw/marts/fact_stops.sql
+++ dbt/models/grupo2_dw/marts/fact_stops.sql
@@ -7,7 +7,7 @@
 
 with
     stops as (
-       select stop_key as sk_stop, stop_id, near_school, m.municipality_key as sk_municipalities, m.region_name
+       select stop_key as sk_stop, stop_id, near_school, m.municipality_key as sk_municipalities, m.region_name, m.municipality_name
        from {{ ref("dim_stops") }} as s
        inner join {{ ref("dim_municipalities") }} as m on m.municipality_id = s.municipality_id
     ),
@@ -26,12 +26,20 @@ with
     stop_line_calendar_date as (
        select ssl.*, cd.calendar_date
        from stop_service_line ssl
-       left join {{ ref("preparation_calendar_dates") }} as cd on ssl.service_id = cd.service_id
+       inner join {{ ref("preparation_calendar_dates") }} as cd on ssl.service_id = cd.service_id
     ),
 
-    stop_line_date as (
-       select slcd.*, date_key as sk_date, day_type_description, period_description
+    stop_line_calendar_date_aggr as (
+            select stop_id, line_id, calendar_date, count(1) as total_line_stops
+            from stop_line_calendar_date
+            group by stop_id, line_id, calendar_date
+    ),
+
+    stop_line_date_aggr as (
+       select slcd.*, sk_date, day_type_description AS day_type, period_description as period, total_line_stops
        from stop_line_calendar_date slcd
+       inner join stop_line_calendar_date_aggr aggr on slcd.stop_id = aggr.stop_id
+       and slcd.line_id = aggr.line_id and aggr.calendar_date = slcd.calendar_date
        left join {{ ref("dim_date") }} as dd on dd.date_day = slcd.calendar_date
     ),
 
@@ -44,14 +52,16 @@ with
 			sk_stop,
 			sk_date,
 			sk_municipalities,
-            day_type_description,
-			period_description,
+            day_type,
+			period,
 			near_school as is_near_school,
+			municipality_name,
 			region_name,
+			total_line_stops,
             -- Controle incremental
             current_timestamp() as created_at,
             current_timestamp() as last_updated_at
-        FROM stop_line_date
+        FROM stop_line_date_aggr
    )
 
 select * from final
