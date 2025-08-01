a968ecc15313b3ba447a2bd3f255674389f5ca31
telmavcosta <tcosta@grouphh.com>
2025-08-01 22:52:58 +0100

    last commit (#34)
    Co-authored-by: telma_costa <telma.costa-ext@compass-group.fr>

--- dbt/models/grupo2_dw/marts/fact_trips.sql
+++ dbt/models/grupo2_dw/marts/fact_trips.sql
@@ -3,20 +3,16 @@
     materialized='table',
 ) }}
 
-{% set surrogate_key_columns = ["trips.trip_id", "stop_times.stop_id"] %}
+{% set surrogate_key_columns = ["trips.trip_id", "stop_times.stop_id", "calendar_dates.sk_date"] %}
 
 with
     stop_times as (
         select
             trip_id,
             stop_id,
---            stop_sequence,
-            is_peak,
-            arrival_time,
-            departure_time,
-            distance,
-            duration_minutes
+            is_peak
         from {{ ref("preparation_gtfs_stop_times") }}
+        GROUP BY trip_id, stop_id, is_peak
     ),
 
     stop_times_agg as (
@@ -28,39 +24,45 @@ with
         group by trip_id
     ),
 
-    trips as (
+    off_peak as (
         select
+            trip_id,
+            CASE
+                WHEN COUNT(DISTINCT is_peak) > 1 THEN true
+                ELSE MAX(is_peak)
+            END AS is_peak_trip
+        from {{ ref("preparation_gtfs_stop_times") }}
+        group by trip_id
+    ),
+
+    trips as (
+        select DISTINCT
             t.trip_id,
             t.service_id,
             t.pattern_id,
-            t.ingestion_date,
-            l.route_key as sk_line,
+            t.route_id as sk_line,
             l.line_name as line_id
         from {{ ref("preparation_trips") }} as t
         inner join {{ ref("dim_lines") }} as l
-            on l.route_id = t.route_id
+        on l.route_id = t.route_id
     ),
 
     calendar_dates as (
-        select service_id, calendar_date
-        from {{ ref("preparation_calendar_dates") }}
-    ),
-
-    dim_date as (
-        select sk_date, date_day
-        from {{ ref("dim_date") }}
+       select distinct service_id, sk_date
+       from {{ ref("preparation_calendar_dates") }} as cd
+       inner join {{ ref("dim_date") }}  as dd
+       ON cd.calendar_date = dd.date_day
     ),
 
     final as (
-        select
+        select DISTINCT
             {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }} as trip_key,
+            trips.trip_id,
             stop_times.stop_id as sk_stop,
             trips.sk_line,
             trips.line_id,
-            dim_date.sk_date as trip_date,
-            stop_times.departure_time,
-            stop_times.arrival_time,
-            stop_times.is_peak as is_peak_time,
+            calendar_dates.sk_date as trip_date,
+            off_peak.is_peak_trip,
             stop_times_agg.total_duration as trip_duration,
             stop_times_agg.total_distance as distance,
             trips.pattern_id,
@@ -68,10 +70,10 @@ with
             current_timestamp() as created_at,
             current_timestamp() as last_updated_at,
         from trips
-        left join stop_times on trips.trip_id = stop_times.trip_id
-        left join calendar_dates on trips.service_id = calendar_dates.service_id
-        left join dim_date on calendar_dates.calendar_date = dim_date.date_day
+        INNER join stop_times on trips.trip_id = stop_times.trip_id
+        INNER join calendar_dates on trips.service_id = calendar_dates.service_id
         inner join stop_times_agg on trips.trip_id = stop_times_agg.trip_id
+        inner join off_peak on trips.trip_id = off_peak.trip_id
     )
 
 select * from final
