e83f56101c12e6abf4108431393da856d1c8e64f
Michel Silva <michel.gomes.silva@gmail.com>
2025-08-01 16:52:03 +0100

    fix pipeline

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -114,29 +114,35 @@ with DAG(
     dbt_run = create_dbt_run_task(IMAGE_URI, ENV_VARS)
 
     # Encadeamento das tarefas
+    ingest_tasks = [
+        ingest_vehicles,
+        ingest_municipalities,
+        ingest_lines,
+        ingest_routes,
+        ingest_stops,
+    ]
+
+    cleanse_basic = [
+        cleanse_lines,
+        cleanse_municipalities,
+        cleanse_stops,
+        cleanse_routes,
+    ]
+
+    cleanse_gtfs = [
+        cleanse_gtfs_periods,
+        cleanse_gtfs_routes,
+        cleanse_gtfs_municipalities,
+        cleanse_gtfs_feed_info,
+        cleanse_gtfs_calendar_dates,
+        cleanse_gtfs_dates,
+    ]
+
     chain(
-        [
-            ingest_vehicles,
-            ingest_municipalities,
-            ingest_lines,
-            ingest_routes,
-            ingest_stops,
-        ],
+        *ingest_tasks,
         ingest_gtfs,
-        [
-            cleanse_lines,
-            cleanse_municipalities,
-            cleanse_stops,
-            cleanse_routes,
-        ],
-        [
-            cleanse_gtfs_periods,
-            cleanse_gtfs_routes,
-            cleanse_gtfs_municipalities,
-            cleanse_gtfs_feed_info,
-            cleanse_gtfs_calendar_dates,
-            cleanse_gtfs_dates,
-        ],
+        *cleanse_basic,
+        *cleanse_gtfs,
         cleanse_gtfs_stops,
         cleanse_gtfs_trips,
         cleanse_gtfs_stop_times,
